ad4195718ffd02f93659a0949d3ae1e89a196f19
Santosh Bhattarai <mr.bullet.sb@gmail.com>
2022-09-02 16:48:26 +0545

    feat: extract metadata from the context of gRPC gateway

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -37,12 +37,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "")
 	}
 
+	mtdt := server.extractMetadata(ctx)
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshTokenPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshTokenPayload.ExpiresAt,
 	})
